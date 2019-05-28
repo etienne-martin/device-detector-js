26210c2f3650332a23e44530fd35d5abc094a0cd
Wbeimar Logatto <wlogatto@merqueo.com>
2019-05-28 14:23:06 -0500

    [Fix] add try catch in userAgentParser , solution crash in safari macos by regexp #31

--- src/utils/user-agent.ts
+++ src/utils/user-agent.ts
@@ -1,6 +1,9 @@
 export const userAgentParser = (rawRegex: string, userAgent: string): string[] | null => {
-  const regex = `(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`;
-  const match = RegExp(regex, "i").exec(userAgent);
-
-  return match ? match.slice(1) : null;
+  try {
+    const regex = `(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`;
+    const match = RegExp(regex, "i").exec(userAgent);
+    return match ? match.slice(1) : null;
+  } catch (error) {
+    return null;
+  }
 };
