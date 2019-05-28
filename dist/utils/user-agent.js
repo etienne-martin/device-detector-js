c560f05ec77467d2cff26d42272ff0325856f1e5
Wbeimar Logatto <wlogatto@merqueo.com>
2019-05-28 14:59:10 -0500

    Build #31

--- dist/utils/user-agent.js
+++ dist/utils/user-agent.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userAgentParser = (rawRegex, userAgent) => {
-    const regex = `(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`;
-    const match = RegExp(regex, "i").exec(userAgent);
-    return match ? match.slice(1) : null;
+    try {
+        const regex = `(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`;
+        const match = RegExp(regex, "i").exec(userAgent);
+        return match ? match.slice(1) : null;
+    }
+    catch (error) {
+        return null;
+    }
 };
