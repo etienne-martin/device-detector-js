cbb694dd4878c67c81c74496773723738c092731
Ayan Dey <ayand04@gmail.com>
2020-01-09 10:35:24 +0530

    Fixed tests

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -4,7 +4,7 @@ export const formatVersion = (version: string | undefined, versionTruncation: 0 
   if (version === undefined) return "";
 
   const versionString = trim(version, ".").replace(new RegExp("_", "g"), ".");
-  const versionParts = versionString.split(".");
+  const versionParts = trim(versionString, " ").split(".");
 
   // Return if the string is not only digits once we removed the dots
   if (!/^\d+$/.test(versionParts.join(""))) {
