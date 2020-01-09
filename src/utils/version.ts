b61b20074cb2f3f4d9d30a5e5a47da05c212eb42
Etienne Martin <github@emartin.ca>
2020-01-09 14:20:52 -0500

    Update version.ts

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -3,8 +3,8 @@ import { trim } from "./trim";
 export const formatVersion = (version: string | undefined, versionTruncation: 0 | 1 | 2 | 3 | null): string => {
   if (version === undefined) return "";
 
-  const versionString = trim(version, ".").replace(new RegExp("_", "g"), ".");
-  const versionParts = trim(versionString, " ").split(".");
+  const versionString = trim(version, ". ").replace(new RegExp("_", "g"), ".");
+  const versionParts = versionString.split(".");
 
   // Return if the string is not only digits once we removed the dots
   if (!/^\d+$/.test(versionParts.join(""))) {
